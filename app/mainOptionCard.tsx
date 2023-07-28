065aa415d34c0a296977c4db384629cd8e30f0a1
kaiser24 <damsog38@gmail.com>
2023-07-27 22:55:56 -0500

    started the project and starting to work on the views

---
+++ app/mainOptionCard.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+interface MainOptionCardProps {
+  title: string;
+  description: string;
+  link: string;
+}
+
+export default function MainOptionCard({title, description, link}: MainOptionCardProps) {
+
+
+  return (
+    <Link href={link}>
+      <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+        <h2 className={`mb-3 text-2xl font-semibold`}>
+          {title}{' '}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          {description}
+        </p>
+      </div>
+    </Link>
+  )
+}

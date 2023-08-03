74e42096cc3c192b3e4df4ab87bcfa7ba608a9a3
kaiser24 <damsog38@gmail.com>
2023-08-03 12:38:31 -0500

    Styling the effect selection

--- app/skills/[skillId]/EffectSelection.tsx
+++ app/skills/[skillId]/EffectSelection.tsx
@@ -50,11 +50,13 @@ export default function EffectSelection({skillId}: EffectSelectionProps) {
       <div className="top-16 h-56 w-full overflow-auto">
         <Listbox value={selected} onChange={setSelected}>
           <div className="relative mt-1">
-            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-[#2b2532] hover:bg-[#3f3847] text-gray-300 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <Listbox.Button className="relative w-full cursor-default rounded-lg bg-black hover:bg-purple-300/10 text-yellow-200/70 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none
+                           focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 
+                           focus-visible:ring-offset-orange-300 sm:text-sm ">
               <span className="block truncate">{selected ? selected.name : "Select A Group"}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <AiOutlineDown
-                  className="h-4 w-4 text-gray-400 text-xs"
+                  className="h-4 w-4 bg-purple-300/10 text-xs"
                   aria-hidden="true"
                 />
               </span>
@@ -65,13 +67,13 @@ export default function EffectSelection({skillId}: EffectSelectionProps) {
               leaveFrom="opacity-100"
               leaveTo="opacity-0"
             >
-              <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-[#2b2532] py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-black py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {query.data.map((effect: any) => (
                   <Listbox.Option
                     key={effect.id}
                     className={({ active }) =>
                       `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                        active ? 'bg-[#3f3847] text-green-700' : 'text-gray-300'
+                        active ? 'bg-purple-400/30 text-yellow-200/70' : 'text-gray-300'
                       }`
                     }
                     value={effect}
@@ -86,7 +88,7 @@ export default function EffectSelection({skillId}: EffectSelectionProps) {
                           {effect.name}
                         </span>
                         {selected ? (
-                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-green-700">
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-yellow-200/70">
                             <AiOutlineCheck className="h-4 w-4" aria-hidden="true" />
                           </span>
                         ) : null}

2613aef048c166a0b2371b9e885fce5d3b8565f5
kaiser24 <damsog38@gmail.com>
2023-08-28 20:19:52 -0500

    Login styles

--- app/login/loginForm.tsx
+++ app/login/loginForm.tsx
@@ -31,12 +31,12 @@ const LoginForm = ({className}: LoginFormClassOptions) => {
     const redirectToSignup = () => router.push('/signup');
  
     return (
-        <div className={`${className} rounded-2xl shadow-lg`}>
-            <form className=' px-16 py-8 bg-[#1d1d23] rounded-2xl flex-row justify-between ' onSubmit={handleSubmit(onSubmit)}>
+        <div className={`${className} rounded-2xl shadow-lg border-4 border-yellow-900/60`}>
+            <form className=' px-16 py-8 bg-[url("/bg1.jpg")] rounded-2xl flex-row justify-between ' onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <input 
                         {...register("email", { required: true })}
-                        className='m-4 rounded-lg p-3 text-gray-400 text-lg bg-[#29292e] focus:bg-[#3c3c40] focus:outline-none'
+                        className='m-4 rounded-lg p-3 text-gray-400 text-lg bg-[#2b2532] bg-opacity-10 focus:bg-opacity-30 focus:outline-none border dark:border-yellow-900/50'
                         type="text"
                         name="email"
                         placeholder="Email"
@@ -45,26 +45,26 @@ const LoginForm = ({className}: LoginFormClassOptions) => {
                 <div>
                     <input
                         {...register("password", { required: true })} 
-                        className='m-4 rounded-lg p-3 text-gray-400 text-lg bg-[#29292e] focus:bg-[#3c3c40] focus:outline-none'
+                        className='m-4 rounded-lg p-3 text-gray-400 text-lg bg-[#2b2532] bg-opacity-10 focus:bg-opacity-30 focus:outline-none border dark:border-yellow-900/50'
                         type="password" 
                         name="password" 
                         placeholder="Password"
                     />
                 </div>
                 <div className='flex flex-col items-center justify-between m-4 hover:text-gray-200
-                                 border border-cyan-600 shadow-lg shadow-cyan-600/50 rounded-lg p-2 hover:bg-[#29292e]
+                                 rounded-md bg-black hover:bg-purple-300/10 border dark:border-yellow-900/50 p-2
                                  active:translate-y-1'>
                     <input type="submit" value="Login" className='text-gray-400 text-lg'/>
                 </div>
                 <div className='px-4 flex flex-col items-center justify-center'>
                     <h5 className='text-gray-400'>Or Sign In using</h5>
                     <div className='my-2 flex space-x-4'>
-                        <h5 className=" text-4xl cursor-pointer text-gray-300 hover:text-cyan-600" onClick={() => signIn()}><AiFillGithub/></h5>
-                        <h5 className=" text-4xl cursor-pointer text-gray-300 hover:text-cyan-600" onClick={() => signIn()}><IoLogoGoogle/></h5>
+                        <h5 className=" text-4xl cursor-pointer text-gray-300 hover:text-yellow-500/80" onClick={() => signIn()}><AiFillGithub/></h5>
+                        <h5 className=" text-4xl cursor-pointer text-gray-300 hover:text-yellow-500/80" onClick={() => signIn()}><IoLogoGoogle/></h5>
                     </div>
                 </div>
                 <div className='px-4 my-2'>
-                    <h1 className="text-gray-400 ">Don't have an account? <span className="text-cyan-600 hover:text-cyan-500 hover:cursor-pointer" onClick={redirectToSignup}>sign-up!</span></h1>
+                    <h1 className="text-gray-400 ">Don't have an account? <span className="text-yellow-500/70 hover:text-yellow-500/90 hover:cursor-pointer" onClick={redirectToSignup}>sign-up!</span></h1>
                 </div>
             </form>
         </div>
